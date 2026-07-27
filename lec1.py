19f23f2055ebefb1aef5f3113b8152df153a69f3
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-07-27 10:53:00 +0530

    Python basics  — Day 1

---
+++ lec1.py
@@ -0,0 +1,10 @@
+print("Hello world");
+
+# "5" is a String (Text)
+print("5+5");
+
+#5 is an Integer (Number)
+print(5+5);
+
+# Output: 55 (Glues the two text characters together end-to-end)
+print ("5"+"5")
