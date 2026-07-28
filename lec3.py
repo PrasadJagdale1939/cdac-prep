5845bb5d10416800ba1734250c677cc4cf0a9f41
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-07-28 11:45:03 +0530

    Data Types-List,Tuple,Dict

---
+++ lec3.py
@@ -0,0 +1,10 @@
+## In Python everything is an object 
+
+list1 = [1 , 2.3,"Prasad", ["prasad", "jagdale"]];
+print(list1);
+
+tuple1 = (("prasad", "jagdale"),("prasad", "jagdale"),"prasad");
+print(tuple1);
+
+dict1 = {"Student":"Prasad", "Age":22, "CanVote":True};
+print(dict1);
