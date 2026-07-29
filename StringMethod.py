4030a653c99fa9ec066e0969858f28ccd335b29e
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-07-29 20:50:45 +0530

    Strings Capitalization

--- StringMethod.py
+++ StringMethod.py
@@ -1,5 +1,5 @@
 #Strings are immutable
-a = "!!!!!Prasad!!!!!!!!!!!!"
+a = "!!!!! Prasad!!!!! !!!!! !!"
 
 print(len(a))
 print(a.upper())
@@ -8,5 +8,14 @@ print(a.lower())
 #it do not effect on leding symbols.
 print(a.rstrip("!"))
 
-#
+#it do  not change the main source
 print(a.replace("Prasad", "Jagdale"))
+
+#
+print(a.split(" "))
+
+#Capitalize()
+
+blogHeading = "welcome tO mY VlOg"
+print(blogHeading.capitalize())
+
