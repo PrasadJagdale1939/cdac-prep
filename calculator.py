411c5a4d2e21e732e1f7f6d1688ef3e07a69024b
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-07-29 17:10:57 +0530

    basic calculater

---
+++ calculator.py
@@ -0,0 +1,10 @@
+a = 50 
+b = 3
+
+
+print("the value of", a, "+", b, " :",a + b )
+print("the value of", a, "-", b, " :", a - b )
+print("the value of", a, "*", b, " :", a * b )
+print("the value of", a, "/", b, " :", a / b )
+print("the value of", a, "//", b, " :", a // b )
+print("the value of", a, "%", b, " :", a % b )
