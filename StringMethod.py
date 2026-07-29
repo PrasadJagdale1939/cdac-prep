1a594699ced17c86385315d1a8d1553c0769f665
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-07-29 21:03:51 +0530

    strings endswith

--- StringMethod.py
+++ StringMethod.py
@@ -1,5 +1,5 @@
 #Strings are immutable
-a = "!!!!! Prasad!!!!! !!!!! !!"
+a = "!!!!! Prasad!!!!! Prasad !!!!!Prasad Prasad Prasad !!"
 
 print(len(a))
 print(a.upper())
@@ -11,11 +11,28 @@ print(a.rstrip("!"))
 #it do  not change the main source
 print(a.replace("Prasad", "Jagdale"))
 
-#
+#split() gives output in list form
 print(a.split(" "))
 
-#Capitalize()
 
+#Capitalize()
 blogHeading = "welcome tO mY VlOg"
 print(blogHeading.capitalize())
+
+
+#Center()
+str1 = "Welcome to my Vlog"
+print(len(str1))
+print(str1.center(50))
+print(len(str1.center(50)))
+
+
+#Count()
+print(a.count("Prasad"))
+
+
+#endswith() -> Always in boolean 
+print(str1.endswith("log"))
+print(str1.endswith("log", 15,18))
+print(str1.endswith("log", 15,17))
 
