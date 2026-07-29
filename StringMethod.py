2843a020a4a35d4c42e69f65e90ed20e0844b713
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-07-29 20:41:22 +0530

    Strings R-strip & Replace

--- StringMethod.py
+++ StringMethod.py
@@ -1,5 +1,12 @@
 #Strings are immutable
-a = "Prasad"
+a = "!!!!!Prasad!!!!!!!!!!!!"
+
 print(len(a))
 print(a.upper())
 print(a.lower())
+
+#it do not effect on leding symbols.
+print(a.rstrip("!"))
+
+#
+print(a.replace("Prasad", "Jagdale"))
