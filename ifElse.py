6f38fa47f49fb5509a18b89efb53f9c28a978568
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-07-31 12:07:22 +0530

    ifElse conditions

---
+++ ifElse.py
@@ -0,0 +1,9 @@
+a = int(input("Enter Your Age "))
+print("Your Age is", a)
+
+#Conditional operators
+# > , < , >= , <= , == , !=
+if(a>=18):
+    print("You can Drive a car")
+else:
+    print("you are not eligible for driving a car")
