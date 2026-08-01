97d7b154d0b7beebb0deac8b46c00a6e9b1c0343
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-01 20:20:12 +0530

    forloops in string and list

---
+++ forloops.py
@@ -0,0 +1,42 @@
+# name = "Prasad"
+# for i in name:
+#     print(i)  #P r a s a d
+#     if(i =="r"):
+#          print("this is something special") #P
+#                                             # r
+#                                             # this is something special
+#                                             # a
+#                                             # s
+#                                             # a
+#                                             # d
+
+colours = ["Red","White","Black","Green"]
+for colour in colours:
+    print(colour)     #Red
+                      #White
+                      #Black
+                      #Green
+    for i in colour:
+        print(i)    #Red
+                    #R
+                    #e
+                    #d
+                    #White
+                    #W
+                    #h
+                    #i
+                    #t
+                    #e
+                    #Black
+                    #B
+                    #l
+                    #a
+                    #c
+                    #k
+                    #Green
+                    #G
+                    #r
+                    #e
+                    #e
+                    #n
+                    
