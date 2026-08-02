d32136cef42f3e971ade3525672ec442094fe61d
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-02 16:33:21 +0530

    emulation of Do-while loop in python

--- breakandcontinue.py
+++ breakandcontinue.py
@@ -3,9 +3,10 @@
 #     if(i ==10):
 #         break   #same for while loop and break statement leave the whole loop
     
-for i in range(1,15,1): #(starting,ending,difference)
-    if(i ==10):
-        print("skip this iteration")
-        continue            # in contioue it just skip that particular iteration
-    print("5 x", i, "=", 5*i)
+# for i in range(1,15,1): #(starting,ending,difference)
+#     if(i ==10):
+#         print("skip this iteration")
+#         continue            # in contioue it just skip that particular iteration
+#     print("5 x", i, "=", 5*i)
    
+
--- whileloop.py
+++ whileloop.py
@@ -23,9 +23,9 @@
 
 #to emulate do while loop in python
 
-i = 0
-while True:
-    print(i)
-    i=i+1
-    if(i%100 ==0):
-        break
+# i = 0
+# while True:
+#     print(i)
+#     i=i+1
+#     if(i%100 ==0):
+#         break
