afaa4500fda979d3c9c48c78239416c20dc35ec7
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-07 19:08:13 +0530

    Tuple operations in python

--- 19operationOnTuple.py
+++ 19operationOnTuple.py
@@ -1,7 +1,16 @@
-countries = ("india","europe","USA","UAE")  #tuple
-temp = list(countries)    #tuple stored in temp as list format
-temp.append("russia")     #we add item
-temp.pop(1)               #we remove item
-temp[2]="finland"         #we change item
-countries= tuple(temp)    #converted tuple in to list , is now again convert into the tuple.
-print(countries)          #print the changed tuple    #('india', 'USA', 'finland', 'russia')
+# countries = ("india","europe","USA","UAE")  #tuple
+# temp = list(countries)    #tuple stored in temp as list format
+# temp.append("russia")     #we add item
+# temp.pop(1)               #we remove item
+# temp[2]="finland"         #we change item
+# countries= tuple(temp)    #converted tuple in to list , is now again convert into the tuple.
+# print(countries)          #print the changed tuple    #('india', 'USA', 'finland', 'russia')
+
+
+
+
+countries1 =("pakistan","afganistan","bangladesh")
+countries2 =("vietnam","india","china")
+
+southEastAsia = countries1 + countries2
+print(southEastAsia)   #('pakistan', 'afganistan', 'bangladesh', 'vietnam', 'india', 'china')
