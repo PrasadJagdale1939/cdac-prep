5950b8b55d953797ca0f70e4e8a1bbb446a94abe
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-07 19:22:50 +0530

    Tuple operations in python

--- 19operationOnTuple.py
+++ 19operationOnTuple.py
@@ -9,8 +9,27 @@
 
 
 
-countries1 =("pakistan","afganistan","bangladesh")
-countries2 =("vietnam","india","china")
+# countries1 =("pakistan","afganistan","bangladesh")
+# countries2 =("vietnam","india","china")
 
-southEastAsia = countries1 + countries2
-print(southEastAsia)   #('pakistan', 'afganistan', 'bangladesh', 'vietnam', 'india', 'china')
+# southEastAsia = countries1 + countries2
+# print(southEastAsia)   #('pakistan', 'afganistan', 'bangladesh', 'vietnam', 'india', 'china')
+
+
+
+
+tuple =(1,2,3,5,3,6,5,3,4,3,3,3,3,3,3,5,2,23,4,3,5)
+# res = tuple.count(3)
+
+# print(res)   #recuursion of that number 3 -> 10
+
+# res2= tuple.index(3)
+# print(res2) # it will give first occurance of 3 is -> on postion 2
+
+# res3 = tuple.index(3,3,9)   #(element,start,end)
+# print(res3)    #it will give first occurance of 3 in the sliced tuple from 3 to 9 is -> on postion 4
+
+
+
+res4 =len(tuple)
+print(res4)   #21
