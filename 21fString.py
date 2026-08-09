8966811d6caa2c26906fc88a0582372076c37f30
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-09 14:13:50 +0530

    f-string(formatted string literal)

---
+++ 21fString.py
@@ -0,0 +1,10 @@
+# f-string -> Formatted string literal
+# the modern, standard way to embed variables and expressions directly inside Python strings.
+
+
+name = "Prasad"
+age = 22
+
+# Basic variable embedding
+print(f"My name is {name} and I am {age} years old.") # Output: My name is Alice and I am 25 years old.
+
