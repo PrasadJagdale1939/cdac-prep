4d4b30a3c18b0d2bc8d0ab8aa74f3016b29527c9
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-10 20:20:55 +0530

    Sets

---
+++ 24Sets.py
@@ -0,0 +1,30 @@
+#In set data is in unordered form
+
+# s = {2,4,2,6}
+# print(s)   #{2, 4, 6}
+
+
+info ={ "Prasad",19,False, 5.9,19}
+print(info)   #{'Prasad', False, 19, 5.9} 
+
+
+for value in info:
+    print(value)    #False
+                    #19
+                    #5.9
+                    #Prasad
+
+
+
+# prasad ={2,3}
+# print(type(prasad))   #<class 'set'>
+
+
+# prasad1 ={ }
+# print(type(prasad1))   #<class 'dict'> #beacuse dictionary syntax is same
+
+
+# prasad3 =set()   #for get a empty Set
+# print(type(prasad3))    #<class 'set'>
+
+
