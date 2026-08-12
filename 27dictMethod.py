0d071f5665c86c7ddd7aa97e5e83e74e60be654a
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-12 20:41:24 +0530

    dictionary Method in Python

--- 27dictMethod.py
+++ 27dictMethod.py
@@ -13,5 +13,24 @@ ep2={653:44,564:78}
 # print(ep1)  #{}  ->empty dictionary
 
 
-empt ={}
-print(empt)
+# empt ={}
+# print(empt)  #{}  ->empty dictionary
+
+
+##########################################################
+
+# ep1.pop(122)
+# print(ep1)    #{52: 34, 367: 70} ---> it has deleted the (122:45)key value pair
+
+
+# ep1.popitem()   #it will pop last key value pair from the dictionary
+# print(ep1)    #{122: 45, 52: 34}
+
+###########################################################
+
+# del ep1  #it will delete the whole dictionary
+# print(ep1) # it will throw an error
+
+del ep1[122]  #it will delete only 122 key with pair
+print(ep1) # {52: 34, 367: 70}
+
