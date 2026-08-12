a7a3f5b654c946f352e5df6005d7a3f3e6937a86
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-12 20:49:50 +0530

    for loops with Else statement

--- 28forloopwithElse.py
+++ 28forloopwithElse.py
@@ -1,7 +1,30 @@
 #For loop
 
+# for i in range(6):
+#     print(i)
+
+# else:
+#     print("sorry no i found")
+    
+
+#########################################################
+
+
+
 for i in range(6):
     print(i)
+    if i ==4:
+        break          #it will break here and do not print the else statement beacause loop is stoped in middle
 
 else:
     print("sorry no i found")
+
+
+
+
+
+
+
+
+
+
