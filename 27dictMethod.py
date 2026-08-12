84e303d4593443493f4a4e6b4d5853d4f89f0756
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-12 20:32:57 +0530

    dictMethod

---
+++ 27dictMethod.py
@@ -0,0 +1,17 @@
+ep1 ={ 122:45,52:34,367:70}
+
+ep2={653:44,564:78}
+
+
+# ep1.update(ep2)  #it will update the ep1 by adding key values of ep2 in ep1
+
+# print(ep1)  #{122: 45, 52: 34, 367: 70, 653: 44, 564: 78}
+
+# print(ep2)  #{653: 44, 564: 78}
+
+# ep1.clear()
+# print(ep1)  #{}  ->empty dictionary
+
+
+empt ={}
+print(empt)
