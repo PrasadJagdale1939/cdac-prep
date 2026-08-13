168ffe0770487ec5dc2ead38ff1b7e702f891075
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-13 20:31:19 +0530

    Custom Error handling

---
+++ 31customErrors.py
@@ -0,0 +1,10 @@
+###ValueError using the raise keyword. This exception is typically used 
+## when a function receives an argument of the correct data type but with 
+## an inappropriate value
+
+a = int(input("Enter any value between 5 and 9 : "))
+
+if(a<5 or a>9):
+    raise ValueError("Value should be between 5 and 9")
+else:
+    print("The given Number is ",a)
