e8d88bfd2988c22a90d5a72c703191a4dbadc497
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-13 20:46:40 +0530

    Custom Error handling

--- 31customErrors.py
+++ 31customErrors.py
@@ -2,9 +2,17 @@
 ## when a function receives an argument of the correct data type but with 
 ## an inappropriate value
 
-a = int(input("Enter any value between 5 and 9 : "))
+a = input("Enter any value between 5 and 9 : ")
 
-if(a<5 or a>9):
-    raise ValueError("Value should be between 5 and 9")
+# 1. Check for the string "quit" first
+if a == "quit":
+    print("program executed successfully")
 else:
-    print("The given Number is ",a)
+    # 2. Convert the input to an integer
+    a = int(a) 
+    
+    # 3. Now you can safely check the numbers
+    if a < 5 or a > 9:
+        raise ValueError("Value should be between 5 and 9")
+    else:
+        print("The given Number is ", a)
