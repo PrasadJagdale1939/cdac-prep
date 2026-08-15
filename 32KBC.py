90f0435d868627dddad78e493a2dfa701c110233
PrasadJagdale1939 <prasadjagdaleofficial@gmail.com>
2026-08-15 18:11:57 +0530

    KBC

---
+++ 32KBC.py
@@ -0,0 +1,14 @@
+questions =[
+    
+    ["Which planet is known as the Red Planet?","Earth","Mars","Venus","Jupiter",2]
+    ["Who wrote the Indian national anthem \"Jana Gana Mana\"?","Bankim Chandra Chatterjee","Rabindranath Tagore","Mahatma Gandhi","Subhash Chandra Bose",2]
+    ["Which animal is known as the Ship of the Desert?","Horse","Elephant","Camel","Donkey",3]
+    ["In which year did India win its first Cricket World Cup under Kapil Dev?","1975","1983","1987","2011",2]
+    ["Which city is known as the Silicon Valley of India?","Mumbai","Hyderabad","Pune","Bengaluru",4]
+    ["Who was the first woman Prime Minister of India?","Pratibha Patil","Sarojini Naidu","Indira Gandhi","Sushma Swaraj",3]
+    ["What is the chemical symbol for Gold?","Ag","Au","Pb","Fe",2]
+    ["Which is the longest river in the world?","Amazon River","Nile River","Yangtze River","Ganga River",2]
+    ["The famous Ajanta Caves are located in which Indian state?","Madhya Pradesh","Bihar","Maharashtra","Odisha",3]
+    ["Who invented the World Wide Web (WWW) in 1989?"," Bill Gates","Steve Jobs","Tim Berners-Lee","Mark Zuckerberg",2]
+              
+            ]
